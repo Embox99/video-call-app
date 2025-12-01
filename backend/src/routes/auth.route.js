30b65298e7d58d6e7a3cce0e8b2997b2eb2dd7cf
Eduard Vilensky <embox53@gmail.com>
2025-12-01 17:29:45 +0200

    user controllers

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,7 +5,7 @@ import {
   logout,
   onboard,
 } from "../controllers/auth.controller.js";
-import protectRoute from "../middleware/auth.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
