5af4720ca0370f6f54f90a6ac2216b8f640a2cb2
Eduard Vilensky <embox53@gmail.com>
2025-12-01 20:38:28 +0200

    add controller for sending friend requests

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/FriendRequest.js";
 import User from "../models/User.js";
 
 export async function getRecommendedUsers(req, res) {
@@ -29,6 +30,53 @@ export async function getMyFriends(req, res) {
     res.status(200).json(user.friends);
   } catch (error) {
     console.error("Error in getMyFriends controller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+export async function sendFriendRequest(req, res) {
+  try {
+    const myId = req.user.id;
+    const { id: recipientId } = req.params;
+    //prevent sending request to yourself
+    if (myId === recipientId)
+      return res
+        .status(400)
+        .json({ message: "You can`t send friend request to yourself" });
+    const recipient = await User.findById(recipientId);
+    if (!recipient) {
+      return res.status(404).json({ message: "Recipient not found-" });
+    }
+    //check if user is already a friends
+    if (recipient.friends.includes(myId)) {
+      return res
+        .status(400)
+        .json({ message: "You are already friend with this user" });
+    }
+    //check if request is already exists
+    const existingRequest = await FriendRequest.findOne({
+      $or: [
+        { sender: myId, recipient: recipientId },
+        {
+          sender: recipientId,
+          recipient: myId,
+        },
+      ],
+    });
+
+    if (existingRequest) {
+      return res.status(400).json({
+        message: "A friend request is already exists between you and this user",
+      });
+    }
+
+    const friendRequest = await FriendRequest.create({
+      sender: myId,
+      recipient: recipientId,
+    });
+
+    res.status(200).json(friendRequest);
+  } catch (error) {
+    console.error("Error in sendFriendRequest controller", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
